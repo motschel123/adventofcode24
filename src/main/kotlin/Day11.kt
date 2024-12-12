03165acc79811f7b497788a71e7a03beb0f4042e
Marcel Schöckel <marcel.schoeckel@gmail.com>
2024-12-12 15:52:13 +0100

    Day 11 Part Two

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,33 +1,71 @@
 package org.example
 
 import java.io.File
+import java.math.BigInteger
+import kotlin.math.log10
 
 fun main() {
-    val input = File("./src/main/resources/day11.txt")
+    val input = File("src/main/resources/day11.txt")
         .readText()
-        .replace("\n", "")
+        .trim()
         .split(" ")
 
-
-    fun compute(times: Int): Int {
-        var nextList = input
-
-        for (i in 1..times) {
-            nextList = nextList.flatMap { num ->
-                if (num == "0")
-                    listOf("1")
-                else if (num.length % 2 == 0)
-                    listOf(
-                        num.substring(0, num.length/2),
-                        num.substring(num.length/2).toInt().toString()
-                    )
-                else
-                    listOf((num.toLong() * 2024).toString())
-            }
+    fun updateStone(stone: String): List<String> {
+        return when {
+            stone == "0" -> listOf("1")
+            stone.length % 2 == 0 -> listOf(
+                    stone.substring(0, stone.length/2),
+                    stone.substring(stone.length/2).toLong().toString()
+            )
+            else -> listOf((stone.toLong() * 2024).toString())
         }
-        return nextList.size
     }
 
-    println("Part One: ${compute(25)}")
-//    println("Part Two: ${compute(75)}")
+    fun calculateStone(stone: String, depth: Int, cache: MutableMap<Pair<String, Int>, ULong>): ULong {
+        if (depth == 0)
+            return 1u
+
+        cache[stone to depth]?.let { return it }
+        if (stone.startsWith("-"))
+            println(stone)
+        return when {
+            stone == "0" -> {
+                val result = calculateStone("1", depth - 1, cache)
+                cache[Pair(stone, depth)] = result
+                result
+            }
+            stone.length % 2 == 0 -> {
+                val nextStones = listOf(
+                    stone.substring(0, stone.length / 2),
+                    stone.substring(stone.length / 2).toLong().toString()
+                )
+
+                val result: ULong = calculateStone(nextStones[0], depth - 1, cache) +
+                        calculateStone(nextStones[1], depth - 1, cache)
+                cache[stone to depth] = result
+                result
+            }
+            else -> {
+                val result = calculateStone((stone.toULong() * 2024u).toString(), depth - 1, cache)
+                cache[stone to depth] = result
+                result
+            }
+
+        }
+    }
+
+    var result1 = input
+    for (i in 1..25)
+        result1 = result1.flatMap(::updateStone)
+    println("Part One ${result1.count()}")
+
+    val globCache1 = mutableMapOf<Pair<String, Int>, ULong>()
+    println(input.sumOf { calculateStone(it, 25, globCache1)})
+
+
+    val globCache = mutableMapOf<Pair<String, Int>, ULong>()
+
+
+    println("Part Two: ")
+    println(input.sumOf { calculateStone(it, 75, globCache)})
 }
